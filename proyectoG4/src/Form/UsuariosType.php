851d22af6feb0567c638fc95483bbf8a89dfce37
victoromar96 <victor.espin@epn.edu.ec>
2019-07-09 13:58:06 -0500

    crud usuarios

---
+++ proyectoG4/src/Form/UsuariosType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Usuarios;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsuariosType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('apellido')
+            ->add('sobrenombre')
+            ->add('password')
+            ->add('telefono')
+            ->add('email')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Usuarios::class,
+        ]);
+    }
+}
